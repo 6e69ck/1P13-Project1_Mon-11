c0c0f8cc4f4a513a77653d4b3e0e83649f05eecd
Nick Ly <nickly88@pm.me>
2024-11-27 15:48:35 -0500

    change qarm offsets

--- main.py
+++ main.py
@@ -60,33 +60,37 @@ def daily_data(passenger_data): #Anannya Pandit
         daily_data.append(current_list)
     return daily_data
 
-def oversold(f_data, d_data): #Nick Ly
+def oversold(flight_data, daily_data): # Nick Ly
     """
     Passenger data is omitted because daily data is derived from passenger data
-    :param list f_data: flight data
-    :param list d_data: daily data
+    :param list flight_data: flight data
+    :param list daily_data: daily data
     """
     os_business_seats = []
     os_economy_seats = []
 
-    for f in f_data:
+    for f in flight_data:
         model = f[0]
-        b_seats = f[1]
-        e_seats = f[2]
+        # Number of total seats on plane
+        business_seats = f[1]
+        eco_seats = f[2]
         gate = f[4]
 
         # Filter data for gate only, convert the filtered data to a list
-        filter_d_data = list(filter(lambda x: x[0] == gate, d_data))
+        filter_d_data = list(filter(lambda x: x[0] == gate, daily_data))
         # make sure the filtered data actually has an item
         if len(filter_d_data) != 0:
             # Grab the element
+            # it is assumed there will be only one plane at the gate, should only be one plane
             d = filter_d_data[0]
+            # Get number of sold seats
             sold_bs = d[1]
             sold_es = d[2]
 
-            # compute oversold ct (if < 0, it means there is actually a surplus of seats)
-            oversold_b = max(0, sold_bs - b_seats )
-            oversold_e = max(0, sold_es - e_seats)
+            # compute oversold count (if < 0, it means there is a surplus of seats, not oversold)
+            # max to clamp down to be >= 0
+            oversold_b = max(0, sold_bs - business_seats )
+            oversold_e = max(0, sold_es - eco_seats)
 
             # Add data to the results
             os_business_seats.append([model, oversold_b])
